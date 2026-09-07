ca296e6abc896dab90de9337d0751ec0d93380d9
Matheus Rodrigues <109880416+donnie1337@users.noreply.github.com>
2026-09-07 20:04:37 -0300

    corrigir e otimizar TPA sem atraso

--- src/main/java/com/donnie1337/essentialsplus/teleport/TeleportService.java
+++ src/main/java/com/donnie1337/essentialsplus/teleport/TeleportService.java
@@ -13,6 +13,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitTask;
 
 import java.util.ArrayList;
 import java.util.Collections;
