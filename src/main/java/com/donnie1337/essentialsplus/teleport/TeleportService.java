406dbcefded0453f0c65e869d0e243dba2f67097
Matheus Rodrigues <109880416+donnie1337@users.noreply.github.com>
2026-09-09 03:44:21 -0300

    Corrige compilação dos botões interativos do TPA

--- src/main/java/com/donnie1337/essentialsplus/teleport/TeleportService.java
+++ src/main/java/com/donnie1337/essentialsplus/teleport/TeleportService.java
@@ -260,7 +260,9 @@ public final class TeleportService implements Listener {
 
     private int nextButtonToken() {
         int token = buttonToken.updateAndGet(value -> value >= 2_000_000_000 ? 1000 : value + 1);
-        while (buttonActions.values().stream().anyMatch(action -> action.token() == token)) token = buttonToken.incrementAndGet();
+        while (buttonActions.values().stream().anyMatch(action -> action.token() == token)) {
+            token = buttonToken.incrementAndGet();
+        }
         return token;
     }
 
