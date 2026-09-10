ff3765aba36c81bc71fa001125545537941e1666
Matheus Rodrigues <109880416+donnie1337@users.noreply.github.com>
2026-09-10 20:20:42 -0300

    feat: make inspected inventories read-only

---
+++ src/main/java/com/donnie1337/essentialsplus/inspect/InspectListener.java
@@ -0,0 +1,23 @@
+package com.donnie1337.essentialsplus.inspect;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryDragEvent;
+
+public final class InspectListener implements Listener {
+
+    @EventHandler
+    public void onInventoryClick(InventoryClickEvent event) {
+        if (event.getView().getTopInventory().getHolder() instanceof InspectHolder) {
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onInventoryDrag(InventoryDragEvent event) {
+        if (event.getView().getTopInventory().getHolder() instanceof InspectHolder) {
+            event.setCancelled(true);
+        }
+    }
+}
