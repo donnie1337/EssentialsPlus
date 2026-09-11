8219a477f45d4a1f1e35e2bb09bec8332b43c37e
Matheus Rodrigues <109880416+donnie1337@users.noreply.github.com>
2026-09-10 21:08:46 -0300

    feat: close live inspection instance with inventory

--- src/main/java/com/donnie1337/essentialsplus/inspect/InspectListener.java
+++ src/main/java/com/donnie1337/essentialsplus/inspect/InspectListener.java
@@ -3,11 +3,18 @@ package com.donnie1337.essentialsplus.inspect;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.inventory.InventoryDragEvent;
 import org.bukkit.event.inventory.InventoryMoveItemEvent;
 import org.bukkit.event.inventory.InventoryPickupItemEvent;
+import org.bukkit.entity.Player;
 
 public final class InspectListener implements Listener {
+    private final LiveInspectionService liveInspectionService;
+
+    public InspectListener(LiveInspectionService liveInspectionService) {
+        this.liveInspectionService = liveInspectionService;
+    }
 
     @EventHandler
     public void onInventoryClick(InventoryClickEvent event) {
@@ -35,6 +42,14 @@ public final class InspectListener implements Listener {
     public void onInventoryPickupItem(InventoryPickupItemEvent event) {
         if (event.getInventory().getHolder() instanceof InspectHolder) {
             event.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onInventoryClose(InventoryCloseEvent event) {
+        if (event.getPlayer() instanceof Player player
+                && event.getInventory().getHolder() instanceof InspectHolder) {
+            liveInspectionService.unregister(player);
         }
     }
 }
