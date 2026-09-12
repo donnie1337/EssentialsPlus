ca3f04b9dc4baf09240fdb75bbd95da82e1a43f7
Matheus Rodrigues <109880416+donnie1337@users.noreply.github.com>
2026-09-11 22:22:44 -0300

    fix: enable tell tab completer

--- src/main/java/com/donnie1337/essentialsplus/EssentialsPlus.java
+++ src/main/java/com/donnie1337/essentialsplus/EssentialsPlus.java
@@ -11,6 +11,7 @@ import com.donnie1337.essentialsplus.command.CraftCommand;
 import com.donnie1337.essentialsplus.command.EcCommand;
 import com.donnie1337.essentialsplus.command.FlyCommand;
 import com.donnie1337.essentialsplus.command.ReplyCommand;
+import com.donnie1337.essentialsplus.command.TellCommand;
 import com.donnie1337.essentialsplus.home.HomeGui;
 import com.donnie1337.essentialsplus.home.HomeService;
 import com.donnie1337.essentialsplus.home.command.DelHomeCommand;
@@ -59,6 +60,7 @@ public final class EssentialsPlus extends JavaPlugin {
         register("v", new VanishCommand(vanishService));
         register("fly", new FlyCommand());
         register("craft", new CraftCommand());
+        register("tell", new TellCommand());
         register("r", new ReplyCommand());
 
         bauService = new BauService(this);
