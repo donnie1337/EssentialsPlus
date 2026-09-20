c0f2790a77dad91c33d3672aa82d6833e1500e6e
Matheus Rodrigues <109880416+donnie1337@users.noreply.github.com>
2026-09-20 04:09:55 -0300

    fix(vanish): update unknown command message

--- src/main/java/com/donnie1337/essentialsplus/vanish/VanishCommand.java
+++ src/main/java/com/donnie1337/essentialsplus/vanish/VanishCommand.java
@@ -25,7 +25,7 @@ public final class VanishCommand implements CommandExecutor, TabCompleter {
             return true;
         }
         if (!player.hasPermission(VANISH_PERMISSION)) {
-            player.sendMessage("§7§lᴄʜᴀᴛ §8• §rComando não encontrado.");
+            player.sendMessage("§c§lᴇʀʀᴏ §8• §rComando não encontrado.");
             return true;
         }
         if (args.length != 0) {
